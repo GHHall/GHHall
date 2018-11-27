021a9fbf073f4cd5cb4427617fa941a82c5c3272
GHHall <ghall@tawacollege.school.nz>
2018-11-27 14:19:00 +1300

    V3

--- Hello.py
+++ Hello.py
@@ -1,4 +1,4 @@
 strName = input("What is your name? ")
 print("Your name is ", strName)
 intAge = int(input("What is your age? "))
-print("Your name is ", strName, "and your age is ", intAge)
+print("Your name is ", strName, "and your age is ", intAge, ".")
